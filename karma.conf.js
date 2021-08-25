e628ea799683df05e5caeabb654021c16ee1b93e
alyson <alyson.nextage@gmail.com>
2021-08-24 22:26:07 -0300

    Correção no login

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular12JwtAuth'),
+      dir: require('path').join(__dirname, './coverage/SoChopp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular12JwtAuth'`, () => {
+  it(`should have as title 'SoChopp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular12JwtAuth');
+    expect(app.title).toEqual('SoChopp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular12JwtAuth app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('SoChopp app is running!');
   });
 });
